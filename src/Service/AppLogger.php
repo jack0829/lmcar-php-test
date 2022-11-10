92307134b7ad03114cd11e69ff8e7fec91db60e4
章程 <jack0829@21cn.com>
2022-11-10 12:22:51 +0800

    errfix

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -11,26 +11,46 @@ class AppLogger
 
     // 日志类型映射，之后可以增加 regist 方法追加日志类型
     protected static array $types = [
-        'log4php' => L\Log4php::class, // 第1项作为默认
+        'log4php' => L\Log4php::class,
         'think-log' => L\ThinkLog::class,
     ];
 
-    // 日志适配器对象
-    private static L\Adapter $logger;
+    // 静态模式 默认日志类型
+    protected static string $type = 'log4php';
 
-    // 执行方法
+    // 静态模式 日志适配器对象
+    private static ?L\Adapter $logger = null;
+
+    // 静态模式 执行方法
     public static function __callStatic($name, $arguments)
     {
-        if (!method_exists(self::$logger, $name)) {
-            call_user_func_array([self::$logger, $name], $arguments);
+        if (self::$logger === null) {
+            self::$logger = self::factory(); // 工厂背后是个单例
         }
+
+        if (!method_exists(self::$logger, $name)) {
+            throw new Exception("日志方法不存在：$name");
+        }
+
+        call_user_func_array([self::$logger, $name], $arguments);
+    }
+
+    // 静态模式 选择日志类型
+    public static function using(string $type)
+    {
+        if (!array_key_exists($type, self::$types)) {
+            throw new Exception("日志类型错误：$type");
+        }
+
+        self::$type = $type;
+        self::$logger = self::factory();
     }
 
     // 工厂方法
-    public static function factory(string $type): Adapter
+    public static function factory(string $type = ''): Adapter
     {
         if (!array_key_exists($type, self::$types)) {
-            $type = self::$types[0];
+            $type = self::$type;
         }
 
         $class = self::$types[$type];
--- tests/Service/AppLoggerTest.php
+++ tests/Service/AppLoggerTest.php
@@ -11,23 +11,51 @@ use App\Service\AppLogger;
 class AppLoggerTest extends TestCase
 {
 
-    public function testInfoLog()
+    public static function setUpBeforeClass(): void
     {
         // 我本机没改时区，其实一般这句都放在应用的入口，调用一次即可
         // date_default_timezone_set('PRC');
 
+        echo '全部日志类型：';
         AppLogger::printAllTypes(); // 打印支持的日志类型
+    }
 
+
+    // 工厂方式
+    public function testLogFactory()
+    {
+
+        // log4php
         $logger = AppLogger::factory('log4php');
-
         $logger->info('This is info log message (log4php)');
         $logger->debug('This is debug log message (log4php)');
         $logger->error('This is error log message (log4php)');
 
+        // think-log
         $logger = AppLogger::factory('think-log');
         $logger->info('This is info log message (think-log)');
         $logger->debug('This is debug log message (think-log)');
         $logger->error('This is error log message (think-log)');
+
+        $this->assertTrue(true);
+    }
+
+    // 静态方式
+    public function testLogStatic()
+    {
+
+        // default
+        AppLogger::info('This is info log message (static default)');
+
+        // log4php
+        AppLogger::using('log4php');
+        AppLogger::info('This is info log message (staticlog4php)');
+        AppLogger::debug('This is debug log message (static log4php)');
+
+        // think-log
+        AppLogger::using('think-log');
+        AppLogger::info('This is info log message (static think-log)');
+        AppLogger::debug('This is debug log message (static think-log)');
 
         $this->assertTrue(true);
     }
